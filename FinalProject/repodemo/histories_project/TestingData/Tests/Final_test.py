c80f54d3aecb40885d7bd5c32c6323412bcf6694
tatevulikhanyan5 <116493848+tatevulikhanyan5@users.noreply.github.com>
2023-11-22 23:13:59 +0400

    Update Final_test.py

--- FinalProject/repodemo/histories_project/TestingData/Tests/Final_test.py
+++ FinalProject/repodemo/histories_project/TestingData/Tests/Final_test.py
@@ -2,3 +2,5 @@ from repodemo.histories_project.TestingData.Tests.BaseTest import BaseTest
 
 
 class Thefinal(BaseTest):
+
+"""Structure is correct, write def test_final(self): and call methods from BaseTest"""
